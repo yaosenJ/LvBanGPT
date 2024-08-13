d797e9ae059388c1f95a23680f9b230fcca69228
Halo. <147613954+yaosenJ@users.noreply.github.com>
2024-08-13 14:13:26 +0800

    Update audio2lip.py

--- text2audio/audio2lip.py
+++ text2audio/audio2lip.py
@@ -1,11 +1,9 @@
 import requests
 import time
 import os
-
-
-api_key = os.environ.get("x_api_key")
-
-
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+api_key =  os.getenv("x_api_key")
 def submit_lipsync_task(video_url, audio_url, model="wav2lip++", synergize=True):
     url = "https://api.synclabs.so/lipsync"
 
